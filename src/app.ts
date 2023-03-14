d39b7b278d02a56c66696bf81e526d222821cadb
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-03-14 15:52:18 -0300

    Untracked changes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express from "express";
-import "dotenv/config";
-import cors from "cors";
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { router } from './routes';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
-
+app.use(router);
 app.listen(PORT, () => console.log(`listen on ${PORT}`));
