8ff2a116a173d9a321bbc17de4a54b69f2914a7b
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-03-14 16:41:26 -0300

    Mongo config

---
+++ src/controllers/item.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+
+const getItem = (req: Request, res: Response) => {
+  try {
+
+  } catch (e) {
+    res.status(500);
+    
+  }
+};
+
+const getItems = (req: Request, res: Response) => {
+
+};
+
+const updateItem = (req: Request, res: Response) => {
+
+};
+
+const addItem = (req: Request, res: Response) => {
+  
+};
+
+const deleteItem = (req: Request, res: Response) => {
+  
+};
+
+export { getItem, getItems, updateItem, addItem, deleteItem };
